cb63977cf952c49a91a5004b599215652e0bc530
David Williamson <david.williamson@varilink.co.uk>
2021-08-02 18:09:59 +0100

    COPY FUNCTION TO OVERRIDE FROM PARENT THEME Copy wp_bootstrap_starter_posted_on function declaration from parent theme into child theme's functions.php ahead of modifying it to modify the parent theme's behaviour. Commit so that subsequent modifications to this function declaration are traceable.

--- theme/functions.php
+++ theme/functions.php
@@ -5,10 +5,10 @@ include get_theme_file_path('/includes/shortcodes.php');
 
 // Enqueue needed scripts
 function needed_styles_and_scripts_enqueue() {
-    
+
     // Add-ons
 
-    
+
     // Custom script
     wp_enqueue_script( 'wpbs-custom-script', get_stylesheet_directory_uri() . '/assets/javascript/script.js' , array( 'jquery' ) );
 
@@ -40,3 +40,34 @@ add_shortcode( 'site_year', 'site_year' );
 //
 // Your code goes below
 //
+function wp_bootstrap_starter_posted_on() {
+  $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+    $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() )
+	);
+
+	$posted_on = sprintf(
+		esc_html_x( 'Posted on %s', 'post date', 'wp-bootstrap-starter' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'wp-bootstrap-starter' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+
+	echo '<span class="posted-on">' . $posted_on . '</span> | <span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+
+  if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+    echo ' | <span class="comments-link"><i class="fa fa-comments" aria-hidden="true"></i> ';
+    /* translators: %s: post title */
+    comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'wp-bootstrap-starter' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+    echo '</span>';
+  }
+
+}
