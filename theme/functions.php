4ce3e68a8945fc0ba647d9ee711c347a0d6a28b7
David Williamson <david.williamson@varilink.co.uk>
2023-12-28 16:05:28 +0000

    INITIAL BLOCK THEME ARTEFACTS
    The block based theme replaces the old theme that was a child theme of
    wp-bootstrap-starter. This new block theme is standalone.

--- theme/functions.php
+++ theme/functions.php
@@ -1,90 +1,17 @@
 <?php
 
-// Include php files
-include get_theme_file_path('/includes/shortcodes.php');
+// Opt-in to more opinionated styles.
 
-// Enqueue needed scripts
-function needed_styles_and_scripts_enqueue() {
+add_action( 'after_setup_theme', function () {
+    add_theme_support( 'align-wide' );
+    add_theme_support( 'wp-block-styles' );
+});
 
-    // Add-ons
+// Enqueue my theme's stylesheet in both the front-end interface.
 
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'site-theme', get_template_directory_uri() . '/style.css'
+    );
+});
 
-    // Custom script
-    wp_enqueue_script( 'wpbs-custom-script', get_stylesheet_directory_uri() . '/assets/js/script.js' , array( 'jquery' ) );
-
-    // enqueue style
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
-
-}
-add_action( 'wp_enqueue_scripts', 'needed_styles_and_scripts_enqueue' );
-
-function cc_mime_types($mimes) {
-$mimes['svg'] = 'image/svg+xml';
-return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
-
-
-add_filter( 'widget_text', 'do_shortcode' );
-
-//Dynamic Year
-function site_year(){
-	ob_start();
-	echo date( 'Y' );
-	$output = ob_get_clean();
-    return $output;
-}
-add_shortcode( 'site_year', 'site_year' );
-
-//
-// Your code goes below
-//
-
-function wp_dequeue_bootstrap_starter_css () {
-  wp_dequeue_style('wp-bootstrap-starter-bootstrap-css');
-}
-  
-function wp_enqueue_bootstrap_starter_child_css () {
-  wp_enqueue_style(
-    'wp-bootstrap-starter-child-bootstrap-css' ,
-    get_template_directory_uri() . '-child/assets/css/bootstrap.min.css'
-  );
-}
-
-add_action ( 'wp_enqueue_scripts' , 'wp_dequeue_bootstrap_starter_css', 11 ) ;
-add_action (
-  'wp_enqueue_scripts' , 'wp_enqueue_bootstrap_starter_child_css'
-) ;
-
-function wp_bootstrap_starter_posted_on() {
-  $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-    $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	}
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() )
-	);
-
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'wp-bootstrap-starter' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'wp-bootstrap-starter' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
-
-  if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-    echo ' | <span class="comments-link"><i class="fa fa-comments" aria-hidden="true"></i> ';
-    /* translators: %s: post title */
-    comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'wp-bootstrap-starter' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
-    echo '</span>';
-  }
-
-}
